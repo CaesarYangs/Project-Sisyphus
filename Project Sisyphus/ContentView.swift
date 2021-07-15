3697785a6c7baa14493b6670ad7229b329996c31
CaesarYang <CaesarYangs@outlook.com>
2021-07-15 13:25:11 +0800

    Initial Commit

---
+++ Project Sisyphus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project Sisyphus
+//
+//  Created by 杨业卿 on 2021/7/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project Sisyphus/Project_SisyphusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_SisyphusApp.swift
+//  Project Sisyphus
+//
+//  Created by 杨业卿 on 2021/7/15.
+//
+
+import SwiftUI
+
+@main
+struct Project_SisyphusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
